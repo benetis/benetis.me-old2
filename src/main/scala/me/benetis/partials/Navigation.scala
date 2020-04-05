d8a36bdaecd45121c56a6dcd78dd31ee602e8a18
Zygimantas <zygimantas@benetis.me>
2020-04-05 12:04:48 +0300

    feat(8queens) update index and nav

--- src/main/scala/me/benetis/partials/Navigation.scala
+++ src/main/scala/me/benetis/partials/Navigation.scala
@@ -7,9 +7,13 @@ import scalatags.Text.all._
 object Navigation extends Partial {
   def render(): Content = {
     tag("nav")(
-      div(
-        a(href := "https://benetis.me/posts/new-world-blog.html",
-          "From the New world")
+      ul(
+        li(
+          a(href := "https://benetis.me/articles/8-queens-backtracking.html",
+            "8 Queens backtracking")),
+        li(
+          a(href := "https://benetis.me/articles/new-world-blog.html",
+            "From the New world")),
       ),
       div(a(href := "https://benetis.me/old", "Archived blog")),
     )
